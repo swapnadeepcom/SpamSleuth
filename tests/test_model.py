ecd1101cb1c21451e512b9f924d85836be2c4014
swapnadeepcom <swapnadeeproy672@gmail.com>
2024-09-25 22:15:58 +0530

    test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,7 +2,7 @@
 
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src/model_inference.py')))
 
 from model_inference import predict  # Import the function to be tested
 
